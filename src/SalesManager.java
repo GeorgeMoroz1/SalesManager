9f43856e9056edb8b160b900f9abd256ac723838
GeorgeMoroz1 <yura0991@mail.ru>
2022-11-04 16:17:59 +0300

    stat added

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,8 +1,21 @@
 public class SalesManager {
     protected int[] sales;
 
+    protected double average = 0;
+    protected int numbers = 0;
+
     public SalesManager(int[] sales) {
         this.sales = sales;
+        if (sales.length > 0) {
+            double sum = 0;
+            for (int i = 0; i < sales.length; i++) {
+                if (sales[i] != min() && sales[i] != max()) {
+                    sum += sales[i];
+                    numbers += 1;
+                }
+            }
+            average = sum / numbers;
+        }
     }
 
     public int max() {
@@ -13,5 +26,15 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
     }
 }
