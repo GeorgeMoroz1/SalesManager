09ec6d416e87fd25d6977dfd7a25dac252c827a2
GeorgeMoroz1 <yura0991@mail.ru>
2022-11-04 15:37:39 +0300

    longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{5, 0, 7});
+        SalesManager salesManager = new SalesManager(new long[]{9, 0, 7});
         System.out.println("Максимальное число продаж: " + salesManager.max());
     }
 }
