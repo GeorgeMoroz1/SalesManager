f4b7542be3cb9597886d7c9bb80a02f2e99d0549
GeorgeMoroz1 <yura0991@mail.ru>
2022-11-07 23:56:45 +0300

    final v2.0

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,21 +1,10 @@
 public class SalesManager {
     protected long[] sales;
-    protected long average = 0;
-    protected long numbers = 0;
 
     public SalesManager(long[] sales) {
         this.sales = sales;
-        if (sales.length > 0) {
-            long sum = 0;
-            for (int i = 0; i < sales.length; i++) {
-                if (sales[i] != min() && sales[i] != max()) {
-                    sum += sales[i];
-                    numbers += 1;
-                }
-            }
-            average = sum / numbers;
-        }
     }
+
 
     public long max() {
         long max = -1;
@@ -36,4 +25,13 @@ public class SalesManager {
         }
         return min;
     }
+
+    public long cutAverage() {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return (sum - max() - min()) / (sales.length - 2);
+    }
 }
+
